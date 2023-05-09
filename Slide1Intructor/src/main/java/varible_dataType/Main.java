d9e401ffe4624c6dde8ff9b658c5d25f335991d1
HungVo2001 <naclub55@gmail.com>
2023-05-09 21:24:56 +0700

    add file

---
+++ Slide1Intructor/src/main/java/varible_dataType/Main.java
@@ -0,0 +1,19 @@
+package varible_dataType;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 7.5f;
+        double d = 20.5;
+        boolean i = true;
+        char samsung = 's';
+        String s = "Hue";
+
+        System.out.println("Số nguyên int a: "+a);
+        System.out.println("Số thực float b: "+b);
+        System.out.println("Số thưc double d: "+d);
+        System.out.println("Kiểu logic: "+i);
+        System.out.println("Kiểu kí tự: "+samsung);
+        System.out.println("Kiểu chuỗi: "+s);
+    }
+}
