eade5a394995859ef10a89f980336d8076766e6e
HungVo2001 <naclub55@gmail.com>
2023-05-19 17:08:18 +0700

    add file

--- Inheritance/src/main/java/point2D/Point2DMain.java
+++ Inheritance/src/main/java/point2D/Point2DMain.java
@@ -7,5 +7,12 @@ public class Point2DMain {
 
         point2D = new Point2D(2.2f,3.2f);
         System.out.println(point2D);
+
+        System.out.println("-----------------");
+        Point3D point3D = new Point3D();
+        System.out.println(point3D);
+
+        point3D = new Point3D(3.3f,4.3f,5.3f);
+        System.out.println(point3D);
     }
 }
