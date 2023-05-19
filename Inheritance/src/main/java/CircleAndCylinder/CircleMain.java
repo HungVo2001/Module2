173f89845bb328fe63a1e16f12a0ae96a616e333
HungVo2001 <naclub55@gmail.com>
2023-05-19 16:17:52 +0700

    add file

---
+++ Inheritance/src/main/java/CircleAndCylinder/CircleMain.java
@@ -0,0 +1,11 @@
+package CircleAndCylinder;
+
+public class CircleMain {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        System.out.println(circle);
+
+        circle = new Circle(3.0,"black");
+        System.out.println(circle);
+    }
+}
---
+++ Inheritance/src/main/java/CircleAndCylinder/CylinderMain.java
@@ -0,0 +1,11 @@
+package CircleAndCylinder;
+
+public class CylinderMain {
+    public static void main(String[] args) {
+        Cylinder cylinder = new Cylinder(1.5);
+        System.out.println(cylinder);
+
+        cylinder = new Cylinder(3.5);
+        System.out.println(cylinder);
+    }
+}
