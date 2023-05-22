3d312879aa000a2f011d646b8b6d898b3f829f1e
HungVo2001 <naclub55@gmail.com>
2023-05-22 16:39:31 +0700

    add file

---
+++ Interface_Abstract/src/main/java/interfaceComparable/Circle.java
@@ -0,0 +1,30 @@
+package interfaceComparable;
+
+public class Circle {
+    private double radius;
+    private String color;
+    private boolean filled;
+    public Circle() {
+
+    }
+    public Circle(double radius){
+        this.radius = radius;
+    }
+    public Circle(double radius, String color, boolean filled) {
+        this.radius = radius;
+        this.color = color;
+        this.filled = filled;
+    }
+    public double getRadius() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
