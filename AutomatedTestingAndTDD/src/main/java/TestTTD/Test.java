29d7336e222bd385eb05b10e26c5209ac6a2153e
HungVo2001 <naclub55@gmail.com>
2023-05-24 11:30:46 +0700

    add file

---
+++ AutomatedTestingAndTDD/src/main/java/TestTTD/Test.java
@@ -0,0 +1,11 @@
+package TestTTD;
+
+public class Test {
+    public static void main(String[] args) {
+        String str = "Hello";
+        char[] arrayChar = str.toCharArray();
+        for (char array : arrayChar) {
+            System.out.println(array);
+        }
+    }
+}
